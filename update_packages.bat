1a0b43dd171ac653d5ff4bc545d3e19214942c78
katanra <dabellstempest@gmail.com>
2026-06-24 14:54:11 -0600

    Initial commit - Cafe Bot with slash commands

---
+++ update_packages.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Updating Cafe Bot packages...
+cd /d "C:\Users\dabel\OneDrive\Desktop\Projects\Cafe Bot"
+pip install -r requirements.txt --upgrade
+echo.
+echo Done! Press any key to close.
+pause
